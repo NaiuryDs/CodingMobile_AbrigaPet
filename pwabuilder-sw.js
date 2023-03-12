5474d7d4876d55c08b6d1cea0025320fb7690b00
NaiuryDs <naiurydutra09@gmail.com>
2023-03-12 10:32:17 -0300

    PWA

---
+++ pwabuilder-sw.js
@@ -0,0 +1,16 @@
+const CACHE = "pwabuilder-offline-v2";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js%27');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+workbox.routing.registerRoute(
+  new RegExp('index.html'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
